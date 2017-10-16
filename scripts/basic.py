efe978fa2bf7e942681de4fa8de2e09cd6eec3b9
Toni Hermoso Pulido <toniher@cau.cat>
2017-10-16 21:19:19 +0200

    revisions

--- scripts/basic.py
+++ scripts/basic.py
@@ -8,6 +8,7 @@ import json
 import mwclient
 import argparse
 import time
+import datetime
 import pprint
 
 parser = argparse.ArgumentParser(description="""Script for testing MediaWiki API""")
@@ -17,56 +18,68 @@ args = parser.parse_args()
 pp = pprint.PrettyPrinter(indent=4)
 
 def main(argv):
-		
-		host = "localhost"
-		user = None
-		password = None
-		protocol = "http"
-		data = {}
-		
-		if args.config:
-				with open(args.config) as json_data_file:
-						data = json.load(json_data_file)
-		
-		if data.has_key("mw"):
-				if data["mw"].has_key("host"):
-								host = data["mw"]["host"]
-				if data["mw"].has_key("user"):
-								user = data["mw"]["user"]
-				if data["mw"].has_key("password"):
-								pwd = data["mw"]["password"]
-				if data["mw"].has_key("protocol"):
-								protocol = data["mw"]["protocol"]
-		
-		site = mwclient.Site((protocol, host))
-		
-		if user and password :
-				# Login parameters
-				site.login(user, pwd)
-				
-		
-		# Example actions ahead
-		
-		# Retrieve content from article
-		# Notice unicode stuff
-		page = site.pages[u'Universitat Autònoma de Barcelona']
-		
-		if page.exists :
-				# Notice if priting in utf8
-				# https://www.mediawiki.org/wiki/API:Main_page -> Retrieve text
-				print page.text().encode('utf8')
-				
-				# Get all links https://www.mediawiki.org/wiki/API:Links
-				# Page object information https://github.com/mwclient/mwclient/blob/master/mwclient/page.py
-				
-				links = page.links()
-				for link in links :
-						print link.name.encode('utf8')
-				
-				# Get all images https://www.mediawiki.org/wiki/API:Images
-				images = page.images()
-				for image in images :
-						print image.name.encode('utf8')
+
+        host = "localhost"
+        user = None
+        password = None
+        protocol = "http"
+        data = {}
+        
+        if args.config:
+                with open(args.config) as json_data_file:
+                        data = json.load(json_data_file)
+        
+        if data.has_key("mw"):
+                if data["mw"].has_key("host"):
+                        host = data["mw"]["host"]
+                if data["mw"].has_key("user"):
+                        user = data["mw"]["user"]
+                if data["mw"].has_key("password"):
+                        pwd = data["mw"]["password"]
+                if data["mw"].has_key("protocol"):
+                        protocol = data["mw"]["protocol"]
+        
+        site = mwclient.Site((protocol, host))
+        
+        if user and password :
+                # Login parameters
+                site.login(user, pwd)
+                        
+        
+        # Example actions ahead
+        
+        # Retrieve content from article
+        # Notice unicode stuff
+        page = site.pages[u'Universitat Autònoma de Barcelona']
+        
+        if page.exists :
+                # Notice if priting in utf8
+                # https://www.mediawiki.org/wiki/API:Main_page -> Retrieve text
+                print page.text().encode('utf8')
+                
+                # Get all links https://www.mediawiki.org/wiki/API:Links
+                # Page object information https://github.com/mwclient/mwclient/blob/master/mwclient/page.py
+                
+                links = page.links()
+                for link in links :
+                        print link.name.encode('utf8')
+                
+                # Get all images https://www.mediawiki.org/wiki/API:Images
+                images = page.images()
+                for image in images :
+                        print image.name.encode('utf8')
+                                
+                # Get all images https://www.mediawiki.org/wiki/API:Images
+                images = page.images()
+                for image in images :
+                        print image.name.encode('utf8')
+
+                # Get all revisions https://www.mediawiki.org/wiki/API:Main_page
+                revisions = page.revisions()
+                for revision in revisions :
+                        dt = datetime.datetime.fromtimestamp( time.mktime( revision['timestamp'] ) )
+                        revtime = '{}'.format(dt.strftime('%F %T'))
+                        print revision['user'].encode('utf8') + " on " + revtime
 
 if __name__ == "__main__":
-		main(sys.argv[1:])
+	main(sys.argv[1:])
