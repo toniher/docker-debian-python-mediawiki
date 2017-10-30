e9b6feae0d772a7504500af18c9711987ec53304
Toni Hermoso Pulido <toniher@cau.cat>
2017-10-30 08:06:27 +0100

    script wikidata

--- scripts/wdbasic.py
+++ scripts/wdbasic.py
@@ -6,7 +6,7 @@ import os
 import os.path
 import json
 import mwclient
-from wikidata.client import Client
+import wikidata.client
 import argparse
 import time
 import datetime
@@ -19,32 +19,51 @@ args = parser.parse_args()
 pp = pprint.PrettyPrinter(indent=4)
 
 def main(argv):
-		
-		
-		host = "localhost"
-		user = None
-		password = None
-		protocol = "http"
-		data = {}
-		
-		if "config" in args:
-				with open(args.config) as json_data_file:
-						data = json.load(json_data_file)
-		
-		if "mw" in data:
-				if "host" in data["mw"]:
-						host = data["mw"]["host"]
-				if "user" in data["mw"]:
-						user = data["mw"]["user"]
-				if "password" in data["mw"]:
-						pwd = data["mw"]["password"]
-				if "protocol" in data["mw"]:
-						protocol = data["mw"]["protocol"]
+        
+        
+        host = "localhost"
+        user = None
+        password = None
+        protocol = "http"
+        lang = "ca"
+        data = {}
+        
+        if "config" in args:
+                with open(args.config) as json_data_file:
+                                data = json.load(json_data_file)
+        
+        if "mw" in data:
+                if "host" in data["mw"]:
+                        host = data["mw"]["host"]
+                if "user" in data["mw"]:
+                        user = data["mw"]["user"]
+                if "password" in data["mw"]:
+                        pwd = data["mw"]["password"]
+                if "protocol" in data["mw"]:
+                        protocol = data["mw"]["protocol"]
+                if "lang" in data["mw"]:
+                        lang = data["mw"]["lang"]
+                        
+        
+        # Search string
+        searchStr = u"Universitat Autònoma de Barcelona"
+        
+        # Connect wikidata
+        site = mwclient.Site((protocol, host))
+
+        
+        # Full text search
+        # https://www.mediawiki.org/wiki/API:Search
+
+        # https://www.wikidata.org/w/api.php?action=query&list=search&srsearch=Albert%20Einstein&utf8=
+
+        
+        # https://www.wikidata.org/w/api.php?action=wbsearchentities&search=".$search."&language=ca&limit=5&format=json";
 
 
-		client = Client()
-		entity = client.get('Q20145', load=True)
-		pp.pprint( entity )
-	
+        client = wikidata.client.Client()
+        entity = client.get('Q20145', load=True)
+        pp.pprint( entity )
+
 if __name__ == "__main__":
-	main(sys.argv[1:])
+        main(sys.argv[1:])
